cc9c6e3553e6721566bb057f0fb134b4fa7e7059
Melissa Chiang <mchiang62@gmail.com>
2019-09-22 10:38:10 -0400

    working on object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,27 +2,51 @@
 // create set of questions, choices, and answers
 // there can only be one answer for each question
 // create a timer
+// correct, incorrect, unanswered
 
 
 
 var quiz = {
     questions: {
         ques1: "Who was the drug sales associate?",
-        ques2: "What is Jason Mendoza's favorite team?" ,
+        ques2: "What was Jason's favorite team?" ,
         ques3: "What was Chidi's profession - ethics prof?" ,
         ques4: "Who was the EDM DJ?",
         ques5: "Who got hit by a truck from picking up the lonely gal margarita mix?",
         ques6: "Who was raised in high society England?" ,
         ques7: "Who died from an air conditioner falling from the 3rd floor and landing on his/her head?" ,
         ques8: "Who provided all the knowledge and information for the humans?" , 
-        ques9: "Who was the Bad Place architect?" ,
+        ques9: "Who was the Bad Place architect in charged of the Good Place?" ,
         ques10: "Who was in the Medium place?" ,
     },
 
     choices: {
+        ques1: [],
+        ques2: [],
+        ques3: [],
+        ques4: [],
+        ques5: [],
+        ques6: [],
+        ques7: [],
+        ques8: [],
+        ques9: [],
+        ques10: [],
 
-    }
+    },
 
+    answers: {
+        ques1: [],
+        ques2: [],
+        ques3: [],
+        ques4: [],
+        ques5: [],
+        ques6: [],
+        ques7: [],
+        ques8: [],
+        ques9: [],
+        ques10: [],
+
+    },
 
 
 
