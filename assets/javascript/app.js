e88a45beca7f41d447805909a7a7fd187a27f483
Melissa Chiang <mchiang62@gmail.com>
2019-09-22 10:00:41 -0400

    questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+// create start button
+// create set of questions, choices, and answers
+// there can only be one answer for each question
+// create a timer
+
+
+
+var quiz = {
+    questions: {
+        ques1: "Who was the drug sales associate?",
+        ques2: "What is Jason Mendoza's favorite team?" ,
+        ques3: "What was Chidi's profession - ethics prof?" ,
+        ques4: "Who was the EDM DJ?",
+        ques5: "Who got hit by a truck from picking up the lonely gal margarita mix?",
+        ques6: "Who was raised in high society England?" ,
+        ques7: "Who died from an air conditioner falling from the 3rd floor and landing on his/her head?" ,
+        ques8: "Who provided all the knowledge and information for the humans?" , 
+        ques9: "Who was the Bad Place architect?" ,
+        ques10: "Who was in the Medium place?" ,
+    },
+
+    choices: {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
